3624695018f82563203a3dd2a41cd21584b68431
Alexandre Cros <alexandre.cros58@gmail.com>
2020-11-06 18:37:08 +0100

    temperatureService, creating roomTemperature class

---
+++ Services/temperatureService/src/main/java/iss/soa/temperatureService/TemperatureServiceApplication.java
@@ -0,0 +1,13 @@
+package iss.soa.temperatureService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureServiceApplication.class, args);
+	}
+
+}
