de8a96df562c413e6194e9b5616ca4c3e3b28dbd
Nwub <andyxu.tls@gmail.com>
2020-11-06 17:46:19 +0100

    Initial commit for light service

---
+++ lightService/src/main/java/iss/soa/lightService/LightServiceApplication.java
@@ -0,0 +1,13 @@
+package iss.soa.lightService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightServiceApplication.class, args);
+	}
+
+}
