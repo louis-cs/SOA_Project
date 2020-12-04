3ccdf8574a40bd5f8078afc1544c1da6c04cbab7
Alexandre Cros <alexandre.cros58@gmail.com>
2020-12-04 11:32:27 +0100

    getting a String value from a sensor on OM2M

--- Services/lightService/src/main/java/iss/soa/lightService/controller/LightResource.java
+++ Services/lightService/src/main/java/iss/soa/lightService/controller/LightResource.java
@@ -2,16 +2,47 @@ package iss.soa.lightService.controller;
 
 import iss.soa.lightService.model.RoomLight;
 
+import java.util.Arrays;
+import org.json.*;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+
 @RestController
 public class LightResource {
 	
 	private RoomLight room = new RoomLight(0);
+	
+	/**
+	 * Trying to get value from OM2M
+	 */
+	@GetMapping
+	public String lightValue() {
+		RestTemplate rt = new RestTemplate();
+		
+		HttpHeaders headers = new HttpHeaders();
+		// Set http request headers
+		headers.set("x-m2m-origin", "admin:admin");
+		headers.set("Content-type", "application/xml");
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		
+		String url = "http://127.0.0.1:8080/~/in-cse/in-name/insaRoomsAE/room1CT/room1SensorsCT/light/DATA/la";
+		
+		ResponseEntity<String> response = rt.exchange(url, HttpMethod.GET, entity, String.class);
+		String ret = null;
+		String s = response.getBody();
+		String[] tokens = s.split(" ");
+		for (String t : tokens) {
+			ret+=t+"\r\n";
+		}
+		return ret;
+	}
 	
 	/**
 	 * Return a RoomLight matching the id
