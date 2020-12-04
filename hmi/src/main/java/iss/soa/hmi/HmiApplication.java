21b48ba1811603acdcbea85385468aafd4e10580
Nwub <andyxu.tls@gmail.com>
2020-12-04 11:28:59 +0100

    Initial commit for human-machine interface

---
+++ hmi/src/main/java/iss/soa/hmi/HmiApplication.java
@@ -0,0 +1,13 @@
+package iss.soa.hmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmiApplication.class, args);
+	}
+
+}
