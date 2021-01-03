d5257a87a9daf94ffbb4a5cc2242562732bda51c
Nwub <andyxu.tls@gmail.com>
2021-01-03 06:01:37 +0100

    Added POST-REST method to OM2M

--- Services/lightService/src/main/java/iss/soa/lightService/controller/LightResource.java
+++ Services/lightService/src/main/java/iss/soa/lightService/controller/LightResource.java
@@ -17,6 +17,7 @@ import org.eclipse.om2m.commons.obix.io.*;
 
 
 @RestController
+@RequestMapping("/light")
 public class LightResource {
 	
 	private RoomLight room = new RoomLight(0);
@@ -40,20 +41,11 @@ public class LightResource {
 		ResponseEntity<String> response = rt.exchange(url, HttpMethod.GET, entity, String.class);
 		String resp = (String)response.getBody();
 		
-		//System.out.println(resp);
-		
 		Mapper mapper = new Mapper();		
 		ContentInstance cin = (ContentInstance)mapper.unmarshal(resp);
 		
-		//System.out.println("------------------------------------");
-		//System.out.println(cin.getContent());
-		//System.out.println("------------------------------------");
-		
 		Obj obj = ObixDecoder.fromString(cin.getContent());
-		//System.out.println(obj.getObjGroup());
 		Int ret = (Int) obj.getObjGroup().get(2);
-		//System.out.println(ret.getVal());
-		//System.out.println(ret.getVal().intValue());
 
 		return ret.getVal().intValue();
 	}
@@ -81,4 +73,38 @@ public class LightResource {
 		return room;
 	}
 	
+	
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Trying to get value from OM2M
+	 */
+	@GetMapping(value = "/set/{val}")
+	public String setValue(@PathVariable String val) {
+		RestTemplate rt = new RestTemplate();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("x-m2m-origin", "admin:admin");
+		headers.set("Content-type", "application/xml;ty=4");
+		
+		String body = "<m2m:cin xmlns:m2m=\"http://www.onem2m.org/xml/protocols\">"
+				+ "    <cnf>message</cnf>"
+				+ "    <con>"
+				+ "      &lt;obj&gt;"
+				+ "        &lt;str name=&quot;appId&quot; val=&quot;light&quot;/&gt;"
+				+ "        &lt;str name=&quot;category&quot; val=&quot;luminosity &quot;/&gt;"
+				+ "        &lt;int name=&quot;data&quot; val=&quot;" + val + "&quot;/&gt;"
+				+ "        &lt;str name=&quot;unit&quot; val=&quot;lux&quot;/&gt;"
+				+ "      &lt;/obj&gt;"
+				+ "    </con>"
+				+ "</m2m:cin>";
+		HttpEntity<String> entity = new HttpEntity<String>(body, headers);
+		
+		String url = "http://127.0.0.1:8080/~/in-cse/in-name/insaRoomsAE/room1CT/room1SensorsCT/light/DATA";
+		
+		ResponseEntity<String> response = rt.exchange(url, HttpMethod.POST, entity, String.class);
+		String resp = (String)response.getBody();
+		return resp;
+	}
 }
