e890789eccb789c2dd6ff8aa7fd6ab4f7a24792c
Nwub <andyxu.tls@gmail.com>
2021-01-07 23:13:24 +0100

    Fixed temperature omission

--- Services/temperatureService/src/main/java/iss/soa/temperatureService/controller/TemperatureResource.java
+++ Services/temperatureService/src/main/java/iss/soa/temperatureService/controller/TemperatureResource.java
@@ -241,4 +241,117 @@ public class TemperatureResource {
 		String resp = (String)response.getBody();
 		return resp;
 	}
+
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Get window state from OM2M
+	 * @param room
+	 * @param id
+	 * @return
+	 */
+	@GetMapping(value = "/radiator", produces = MediaType.APPLICATION_JSON_VALUE)
+	public boolean getRadiator(@RequestParam String room, @RequestParam String id){
+		RestTemplate rt = new RestTemplate();		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("x-m2m-origin", "admin:admin");
+		headers.set("Content-type", "application/xml");
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		
+		String url = "http://127.0.0.1:8080/~/in-cse/in-name/insaRoomsAE/room"
+				+ room + "CT/room" + room + "ActuatorsCT/radiator" + id + "/la";
+		
+		ResponseEntity<String> response = rt.exchange(url, HttpMethod.GET, entity, String.class);
+		String resp = (String)response.getBody();
+		
+		Mapper mapper = new Mapper();		
+		ContentInstance cin = (ContentInstance)mapper.unmarshal(resp);
+		
+		Obj obj = ObixDecoder.fromString(cin.getContent());
+		Bool ret = (Bool) obj.getObjGroup().get(2);
+
+		return ret.getVal().booleanValue();		
+	}
+
+	/**
+	 * Set window state to OM2M
+	 * @param room
+	 * @param id
+	 * @param val
+	 * @return
+	 */
+	@GetMapping(value = "/radiator/set", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String setRadiator(@RequestParam String room, @RequestParam String id, @RequestParam String val){
+		RestTemplate rt = new RestTemplate();		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("x-m2m-origin", "admin:admin");
+		headers.set("Content-type", "application/xml;ty=4");
+		
+		String body = "<m2m:cin xmlns:m2m=\"http://www.onem2m.org/xml/protocols\">"
+				+ "    <cnf>message</cnf>"
+				+ "    <con>"
+				+ "      &lt;obj&gt;"
+				+ "        &lt;str name=&quot;appId&quot; val=&quot;temperature&quot;/&gt;"
+				+ "        &lt;str name=&quot;category&quot; val=&quot;radiator&quot;/&gt;"
+				+ "        &lt;bool name=&quot;data&quot; val=&quot;" + val + "&quot;/&gt;"
+				+ "        &lt;str name=&quot;unit&quot; val=&quot;boolean&quot;/&gt;"
+				+ "      &lt;/obj&gt;"
+				+ "    </con>"
+				+ "</m2m:cin>";		
+		
+		HttpEntity<String> entity = new HttpEntity<String>(body, headers);		
+		String url = "http://127.0.0.1:8080/~/in-cse/in-name/insaRoomsAE/room"
+				+ room + "CT/room" + room + "ActuatorsCT/window" + id;
+		
+		ResponseEntity<String> response = rt.exchange(url, HttpMethod.POST, entity, String.class);
+		String resp = (String)response.getBody();
+		return resp;
+	}
+	
+	/**
+	 * Create window
+	 * @param room
+	 * @param id
+	 * @return
+	 */	
+	@GetMapping(value = "/radiator/create", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String radiatorCreate(@RequestParam String room, @RequestParam String id){
+		RestTemplate rt = new RestTemplate();		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("x-m2m-origin", "admin:admin");
+		headers.set("Content-type", "application/xml;ty=3");
+		
+		String body = "<m2m:cnt xmlns:m2m=\"http://www.onem2m.org/xml/protocols\" rn=\"radiator"+id+"\"></m2m:cnt>";		
+		
+		HttpEntity<String> entity = new HttpEntity<String>(body, headers);		
+		String url = "http://127.0.0.1:8080/~/in-cse/in-name/insaRoomsAE/room"
+				+ room + "CT/room" + room + "ActuatorsCT";
+		
+		ResponseEntity<String> response = rt.exchange(url, HttpMethod.POST, entity, String.class);
+		String resp = (String)response.getBody();
+		return resp;
+	}
+	
+	/**
+	 * Delete window
+	 * @param room
+	 * @param id
+	 * @return
+	 */	
+	@GetMapping(value = "/radiator/delete", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String radiatorDelete(@RequestParam String room, @RequestParam String id){
+		RestTemplate rt = new RestTemplate();		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("x-m2m-origin", "admin:admin");
+		headers.set("Content-type", "application/xml;ty=3");
+		
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		String url = "http://127.0.0.1:8080/~/in-cse/in-name/insaRoomsAE/room"
+				+ room + "CT/room" + room + "ActuatorsCT/radiator" + id;
+		
+		ResponseEntity<String> response = rt.exchange(url, HttpMethod.DELETE, entity, String.class);
+		String resp = (String)response.getBody();
+		return resp;
+	}
 }
